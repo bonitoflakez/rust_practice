5b9e66d49924cfb9343b958226295ebbd4539ba6
Bonito <nikhildhiman9050@gmail.com>
2023-07-02 12:51:08 +0530

    ch 5.3

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -17,6 +17,29 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     {
         let user1 = build_user(
@@ -76,6 +99,39 @@ fn main() {
 
         // println!("rect1 is {:?}", rect1);
         dbg!(&rect1);
+    }
+
+    {
+        let rect1 = Rectangle {
+            width: 30,
+            height: 50,
+        };
+
+        if rect1.width() {
+            println!("rectangle has a nonzero width; it is {}", rect1.width);
+        }
+
+        println!("area of rectangle is {} square pixels.", rect1.area())
+    }
+
+    {
+        let rect1 = Rectangle {
+            width: 30,
+            height: 50,
+        };
+
+        let rect2 = Rectangle {
+            width: 10,
+            height: 40,
+        };
+
+        let rect3 = Rectangle {
+            width: 60,
+            height: 45,
+        };
+
+        println!("can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+        println!("can rect1 hold rect3? {}", rect1.can_hold(&rect3));
     }
 }
 
