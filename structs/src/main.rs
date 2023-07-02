7f1b2961fc487799cb045f89a4823aa2bda9a0ef
Bonito <nikhildhiman9050@gmail.com>
2023-07-02 10:01:08 +0530

    ch 5.2

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -11,6 +11,12 @@ struct Point(i32, i32, i32);
 
 struct AlwaysEqual;
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     {
         let user1 = build_user(
@@ -34,6 +40,55 @@ fn main() {
     {
         let subject = AlwaysEqual;
     }
+
+    // example program using structs
+    {
+        let width1 = 30;
+        let height1 = 50;
+
+        println!(
+            "are of rectangle is {} square pixels.",
+            area(width1, height1)
+        )
+    }
+
+    {
+        let rect1 = (30, 50);
+
+        println!("area of rectangle is {} square pixels.", new_ar(rect1))
+    }
+
+    {
+        let rect1 = Rectangle {
+            width: 30,
+            height: 50,
+        };
+
+        println!("area of rectangle is {} square pixels.", new_ar_2(&rect1))
+    }
+
+    {
+        let scale = 2;
+        let rect1 = Rectangle {
+            width: dbg!(30 * scale),
+            height: 50,
+        };
+
+        // println!("rect1 is {:?}", rect1);
+        dbg!(&rect1);
+    }
+}
+
+fn new_ar_2(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+fn new_ar(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
 
 fn build_user(email: String, username: String) -> User {
