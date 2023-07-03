082c56e8fae39dd46fbf1f67437e672259673e7b
Bonito <nikhildhiman9050@gmail.com>
2023-07-04 02:04:26 +0530

    ch 8.2

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -24,13 +24,13 @@ fn main() {
         }
     }
 
-    {
-        // PANICS!!
-        let v = vec![1, 2, 3, 4, 5];
+    // {
+    // PANICS!!
+    //     let v = vec![1, 2, 3, 4, 5];
 
-        let does_not_exist = &v[100];
-        let does_not_exist = v.get(100);
-    }
+    //     let does_not_exist = &v[100];
+    //     let does_not_exist = v.get(100);
+    // }
 
     // iteration
     {
@@ -60,5 +60,63 @@ fn main() {
             SpreadsheetCell::Text(String::from("blue")),
             SpreadsheetCell::Float(10.12),
         ];
+    }
+
+    {
+        let mut s = String::new();
+    }
+
+    {
+        let data = "initial contents";
+
+        let s = data.to_string();
+
+        let s = "initial contents".to_string();
+    }
+
+    // updating a string
+    {
+        let mut s = String::from("foo");
+        s.push_str("bar");
+    }
+
+    {
+        let mut s1 = String::from("foo");
+        let s2 = "bar";
+        s1.push_str(s2);
+        println!("s2 is {s2}");
+    }
+
+    {
+        let mut s = String::from("lo");
+        s.push('l');
+    }
+
+    {
+        let s1 = String::from("hello");
+        let s2 = String::from("world");
+        let s3 = s1 + &s2;
+    }
+
+    {
+        let s1 = String::from("tic");
+        let s2 = String::from("tac");
+        let s3 = String::from("toe");
+
+        let s = format!("{s1}-{s2}-{s3}");
+        println!("{s}");
+    }
+
+    {
+        let str = "yellow";
+
+        let mut counter = 0;
+
+        for c in str.chars() {
+            counter += 1;
+            print!("{c}");
+        }
+
+        println!("\nstr has {counter} characters")
     }
 }
